8ef134a25a42148dc78e3e5437380aa6b55858f5
legrandc88 <legrandc8@gmail.com>
2018-04-18 16:39:59 +0200

    insertion projet service windows vierge

--- y2kbugTest/classtest1.cs
+++ y2kbugTest/classtest1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.fakes;
+using System.Fakes;//D'où vient System.Fakes?
 using Microsoft.QualityTools.Testing.Fakes;
 
 namespace y2kbugTest
@@ -15,8 +15,10 @@ namespace y2kbugTest
             using (ShimsContext.Create())
             
             { 
-            ShimDateTime.NowGet=()=>new DateTime(2000,1,1);
-                y2kbug.Check();
+            //jour 21 pour tester le repérage de la campagne de remboursement
+            System.Fakes.ShimDateTime.NowGet= () => {return new DateTime(2018,4,21);
+              
+            PPE_GSB_MISSION.GesDates.verifDates();//pour signaler la méthode concernée
             }
 
         }
